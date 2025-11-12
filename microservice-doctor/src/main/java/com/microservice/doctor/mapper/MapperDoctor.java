036a4d8ca404b9810bd7e2545dce9db809255155
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-12 17:35:31 +0100

    feat(doctor-service): crear interfaz de MapperDoctor

---
+++ microservice-doctor/src/main/java/com/microservice/doctor/mapper/MapperDoctor.java
@@ -0,0 +1,14 @@
+package com.microservice.doctor.mapper;
+
+import com.microservice.doctor.dtos.DoctorDTO;
+import com.microservice.doctor.dtos.DoctorRequest;
+import com.microservice.doctor.model.Doctor;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface MapperDoctor {
+    Doctor toEntity(DoctorRequest request);
+
+    DoctorDTO toDto(Doctor doctor);
+
+}
