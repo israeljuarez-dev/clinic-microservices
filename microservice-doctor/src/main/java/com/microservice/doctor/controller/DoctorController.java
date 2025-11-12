6c644c234ce98677b3f6fb2313bcceb5f2d3ac99
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-12 17:51:25 +0100

    feat(doctor-service): crear endpoint reactivo para agregar un doctor con mapper

--- microservice-doctor/src/main/java/com/microservice/doctor/controller/DoctorController.java
+++ microservice-doctor/src/main/java/com/microservice/doctor/controller/DoctorController.java
@@ -1,4 +1,28 @@
 package com.microservice.doctor.controller;
 
+import com.microservice.doctor.dtos.DoctorDTO;
+import com.microservice.doctor.dtos.DoctorRequest;
+import com.microservice.doctor.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/doctor")
 public class DoctorController {
+    @Autowired
+    private DoctorService doctorService;
+
+    @PostMapping("/add")
+    public Mono<ResponseEntity<DoctorDTO>> createDoctor(@RequestBody Mono<DoctorRequest> doctorRequestMono) {
+        return doctorRequestMono
+                .flatMap(doctorService::createDoctor) // todo lo hace el service
+                .map(dto -> ResponseEntity.status(HttpStatus.CREATED).body(dto));
+    }
+
 }
--- microservice-doctor/src/main/java/com/microservice/doctor/service/DoctorService.java
+++ microservice-doctor/src/main/java/com/microservice/doctor/service/DoctorService.java
@@ -1,15 +1,30 @@
 package com.microservice.doctor.service;
 
 
+import com.microservice.doctor.dtos.DoctorDTO;
+import com.microservice.doctor.dtos.DoctorRequest;
+import com.microservice.doctor.mapper.MapperDoctor;
+import com.microservice.doctor.model.Doctor;
 import com.microservice.doctor.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class DoctorService {
     @Autowired
     private DoctorRepository doctorRepository;
 
+    @Autowired
+    private MapperDoctor mapperDoctor;
+
     /// ---- Operaciones ABML ----
+    public Mono<DoctorDTO> createDoctor(DoctorRequest request) {
+        Doctor doctor = mapperDoctor.toEntity(request);
+        return doctorRepository.save(doctor)
+                .map(mapperDoctor::toDto);
+    }
+
+
 
 }
