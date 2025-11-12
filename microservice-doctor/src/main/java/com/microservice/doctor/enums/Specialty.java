77a45e0366f2804fded938c433f16faf26cc4f37
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-12 15:20:59 +0100

    feat(doctor-service): crear entidad Doctor con campos según consigna

---
+++ microservice-doctor/src/main/java/com/microservice/doctor/enums/Specialty.java
@@ -0,0 +1,4 @@
+package com.microservice.doctor.enums;
+
+public enum Specialty {
+}
--- microservice-doctor/src/main/java/com/microservice/doctor/model/Doctor.java
+++ microservice-doctor/src/main/java/com/microservice/doctor/model/Doctor.java
@@ -1,10 +1,29 @@
 package com.microservice.doctor.model;
 
-import jdk.jfr.DataAmount;
 
-import java.lang.annotation.Documented;
+import com.microservice.doctor.enums.Specialty;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @Data
 @Document(collection= "doctor")
 public class Doctor {
+    @Id
+    private String id_doctor;
+    private String name;
+    private String last_name;
+    private Long dni;
+    private LocalDate birth_date;
+    private String email;
+    private Long phone_number;
+    private String address;
+
+    private Specialty specialty;
+    private Set<LocalDateTime> available_appointments;
+    private Double salary;
 }
