c6db0feb3dcf62a3dec95c28bef9f09e96ab8e2c
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-12 17:31:31 +0100

    feat(doctor-service): crear DTO para agregar un doctor

--- microservice-doctor/src/main/java/com/microservice/doctor/service/DoctorService.java
+++ microservice-doctor/src/main/java/com/microservice/doctor/service/DoctorService.java
@@ -1,4 +1,18 @@
 package com.microservice.doctor.service;
 
+import com.microservice.doctor.dtos.DoctorDTO;
+import com.microservice.doctor.dtos.DoctorRequest;
+import com.microservice.doctor.model.Doctor;
+import com.microservice.doctor.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
 public class DoctorService {
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    /// ---- Operaciones ABML ----
+
 }
