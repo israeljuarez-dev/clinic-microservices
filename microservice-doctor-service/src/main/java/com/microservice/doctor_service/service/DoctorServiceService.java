2e18c025a668e66e2b3a9b2fd28ec1532e1d1912
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-14 18:29:54 +0100

    feat(doctor-service-service): crear DoctorServiceRequest para agregar un medico

--- microservice-doctor-service/src/main/java/com/microservice/doctor_service/service/DoctorServiceService.java
+++ microservice-doctor-service/src/main/java/com/microservice/doctor_service/service/DoctorServiceService.java
@@ -1,11 +1,18 @@
 package com.microservice.doctor_service.service;
 
+import com.microservice.doctor_service.dtos.DoctorServiceDTO;
+import com.microservice.doctor_service.model.DoctorServiceRequest;
 import com.microservice.doctor_service.repository.DoctorServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class DoctorServiceService {
     @Autowired
     private DoctorServiceRepository repository;
+
+    /// ---- Operaciones ABML ------
+
+   
 }
